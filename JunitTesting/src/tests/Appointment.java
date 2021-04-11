c37bfa39636b0a687b571bd6815f2cc9e19ffbb0
Katie Stapleton <Deepdarkstar7@gmail.com>
2021-04-10 23:08:38 -0500

    Removed completed pseudocode for appt dates

--- JunitTesting/src/tests/Appointment.java
+++ JunitTesting/src/tests/Appointment.java
@@ -40,53 +40,6 @@ public class Appointment {
 			apptDate = null;
 		}
 		this.apptDate = dateAppt;
-		
-	/*	if (dateAppt.isBlank()) {
-			dateAppt = null;
-		}
-	*/	
-		
-		/* TASK: compare dates of user input vs local date now (w/ mins)
-		 *   user input must be equal or after local date now (w/ mins)
-		 *   ALT: local date must be before or equal to user input
-		 *   
-		 *   if value is not future tense, date is null
-		 *   current/future dates are valid
-		 *  
-		 *  troubleshooter: 
-		 *  https://docs.oracle.com/javase/9/docs/api/java/time/class-use/LocalDate.html
-		 *  https://stackoverflow.com/questions/42522744/taking-input-using-localdate
-		 *  https://beginnersbook.com/2017/10/java-localdate-compareto-method-example/
-		 *  https://stackoverflow.com/questions/29201103/how-to-compare-localdate-instances-java-8
-		 *  https://www.callicoder.com/how-to-compare-date-time-java/
-		 *  
-		 *  declare current time and (input) appt date. LocalDate
-		 *  create date formatter for pattern
-		 *  retrieve input
-		 *  change input into appt date
-		 *  compare appt date to present date (method)
-		 *  use results to calculate outcome. 
-		 *  apptDate.compareto(localdate)
-		 *  IF results = 0. same day
-		 *  IF results > 0. future day
-		 *  IF results < 0. past day
-
-LocalDate newDate = dateInput(stringInput("Enter a date (like 3/3/17): "));
-public static LocalDate dateInput(String userInput) {
-
-    DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("M/d/yyyy");
-    LocalDate date = LocalDate.parse(userInput, dateFormat);
-
-
-    System.out.println(date);
-    return date ;
-}
-		 */
-		
-		//else if (dateAppt IN PAST ) {
-			
-		//}
-		//this.apptDate = dateAppt;
 	}
 
 
